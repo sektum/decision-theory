88f20649b20b07d450fb9d844420613bcfb78a3a
Valeriia Grankina <grankina@Valeriias-MacBook-Pro.local>
2018-05-15 22:55:01 +0300

    Added lpr & criterion tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,27 @@
+delete from ALTERNATIVE;
 INSERT INTO ALTERNATIVE (IDALT, ANAME) VALUES (1, 'HP 2017 15.6" Full-HD IPS UWVA Laptop');
 INSERT INTO ALTERNATIVE (IDALT, ANAME) VALUES (2, 'Dell Latitude E7470 Business Ultrabook');
 INSERT INTO ALTERNATIVE (IDALT, ANAME) VALUES (3, 'Apple MacBook Pro MF839LL/A');
 INSERT INTO ALTERNATIVE (IDALT, ANAME) VALUES (4, 'Lenovo Yoga');
 INSERT INTO ALTERNATIVE (IDALT, ANAME) VALUES (5, 'ASUS K501UW-AB78');
+
+delete from LPR;
+insert into LPR (LNAME, LRANGE) values ('Иван Иванов', 1);
+insert into LPR (LNAME, LRANGE) values ('Петр Петров', 1);
+insert into LPR (LNAME, LRANGE) values ('Василий Сидоров', 1);
+
+delete from CRITERION;
+insert into CRITERION (CNAME, CRANGE, CWEIGHT, CTYPE, OPTIMTYPE, EDIZMER, SCALETYPE)
+VALUES ('Диагональ экрана', 10, 10, 'Количественный', 'Неприменимо', 'Дюймы', 'Абсолютная');
+insert into CRITERION (CNAME, CRANGE, CWEIGHT, CTYPE, OPTIMTYPE, EDIZMER, SCALETYPE) VALUES (
+    'Вес', 5, 5, 'Количественный', 'Min', 'Килограммы', 'Абсолютная'
+);
+insert into CRITERION (CNAME, CRANGE, CWEIGHT, CTYPE, OPTIMTYPE, EDIZMER, SCALETYPE) VALUES (
+    'Батарея', 10, 10, 'Количественный', 'Max', 'Часы', 'Абсолютная'
+);
+insert into CRITERION (CNAME, CRANGE, CWEIGHT, CTYPE, OPTIMTYPE, EDIZMER, SCALETYPE) VALUES (
+    'Производитель', 2, 2, 'Качественный', 'Неприменимо', 'Неприменимо', 'Номинальная'
+);
+insert into CRITERION (CNAME, CRANGE, CWEIGHT, CTYPE, OPTIMTYPE, EDIZMER, SCALETYPE) VALUES (
+    'Процессор', 5, 5, 'Качественный', 'Max', 'Неприменимо', 'Порядковая'
+);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,25 @@
 -- MUST BE RUN MANUALLY AGAINST DATABASE
-drop table Alternative;
+drop table if exists Alternative;
 create table Alternative(
-  IdAlt bigint(20) NOT NULL auto_increment,
+  IdAlt bigint(20) NOT NULL auto_increment primary key,
   AName varchar(255) NOT NULL,
+);
+
+drop table if exists lpr;
+create table lpr (
+  idLPR bigint(20) not null auto_increment primary key,
+  LName varchar(255) not null,
+  LRange smallint not null
+);
+
+drop table if exists Criterion;
+create table Criterion (
+  IdCrit bigint(20) not null auto_increment primary key ,
+  CName varchar(255) not null,
+  CRange int8 not null,
+  CWeight int8 not null,
+  CType varchar(255) not null,
+  OptimType varchar(255) not null,
+  EdIzmer varchar(255) not null,
+  ScaleType varchar(255) not null
 );
