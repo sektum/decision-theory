1ed9fbcb4e8ade73569a0ac2e2edf3090e112ea4
Michael Groys <mihailgroys@gmail.com>
2018-05-26 18:05:41 +0300

    Add dataLpr.sql script for LPR table. Need to execute.

---
+++ src/main/resources/dataLpr.sql
@@ -0,0 +1,8 @@
+delete from LPR;
+insert into LPR (IDLPR, LNAME, LRANGE) values (1, 'Кирилл Карачевцев', 1);
+insert into LPR (IDLPR, LNAME, LRANGE) values (2, 'Петр Порошенко', 2);
+insert into LPR (IDLPR, LNAME, LRANGE) values (3, 'Никита Джигурда', 1);
+insert into LPR (IDLPR, LNAME, LRANGE) values (4, 'Виктор Пинкин', 2);
+insert into LPR (IDLPR, LNAME, LRANGE) values (5, 'Скарлетт Йохансон', 1);
+insert into LPR (IDLPR, LNAME, LRANGE) values (6, 'Мелисса Бенойст', 1);
+insert into LPR (IDLPR, LNAME, LRANGE) values (7, 'Натали Дормер', 1);
